6ca621e56d422b4eac62e238e48b08df3caad70a
Sajedul Karim <mesukcse08@gmail.com>
2019-11-20 22:54:00 +0600

    Docker file added

--- src/main/java/com/skarim/product/ProductController.java
+++ src/main/java/com/skarim/product/ProductController.java
@@ -1,22 +1,42 @@
 package com.skarim.product;
 
 
+import com.skarim.product.object.MainResponse;
 import com.skarim.product.object.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private Environment environment;
+
     @RequestMapping(method = RequestMethod.GET, value="/")
-    public ResponseEntity<List<Product>> hello(){
+    public ResponseEntity<MainResponse> hello(){
+
+        MainResponse mainResponse=new MainResponse();
+
+        String serverName=environment.getProperty("spring.application.name");
+
+        String ipInfo=getIpInfo();
+
+        mainResponse.setServerName(serverName);
+        mainResponse.setIpInfo(ipInfo);
+        mainResponse.setDateTime(new Date().toString());
+
 
         List<Product> productList=new ArrayList<>();
 
@@ -28,10 +48,36 @@ public class ProductController {
         product=new Product("1003","Computer");
         productList.add(product);
 
+        mainResponse.setProducts(productList);
+
 
         return new ResponseEntity<>(
-                productList,
+                mainResponse,
                 HttpStatus.OK);
+    }
+
+    private String getIpInfo(){
+
+        String ipInfo="";
+
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            ipInfo+=("Your current IP address : " + ip)+"\n";
+            ipInfo+=("Your current Hostname : " + hostname);
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+
+            ipInfo+=e.getMessage();
+
+        }
+
+        return ipInfo;
+
     }
 
 }
---
+++ src/main/java/com/skarim/product/object/MainResponse.java
@@ -0,0 +1,47 @@
+package com.skarim.product.object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainResponse {
+    private String serverName;
+    private String ipInfo;
+    private String dateTime;
+    private List<Product> products;
+
+    public MainResponse() {
+        products=new ArrayList<>();
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getIpInfo() {
+        return ipInfo;
+    }
+
+    public void setIpInfo(String ipInfo) {
+        this.ipInfo = ipInfo;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
