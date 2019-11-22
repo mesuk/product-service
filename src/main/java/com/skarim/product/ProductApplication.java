b00f26e5454ec75b58d50bcdea94ff8cb4f5c0b9
Sajedul Karim <mesukcse08@gmail.com>
2019-11-22 23:09:55 +0600

    eureka instance config added

--- src/main/java/com/skarim/product/ProductApplication.java
+++ src/main/java/com/skarim/product/ProductApplication.java
@@ -1,13 +1,8 @@
 package com.skarim.product;
 
-import com.netflix.appinfo.AmazonInfo;
-import com.skarim.product.config.EurekaInstanceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 
 @EnableDiscoveryClient
@@ -18,11 +13,5 @@ public class ProductApplication {
         SpringApplication.run(ProductApplication.class, args);
     }
 
-
-    @Bean
-    @Profile({"dev", "dc"})
-    public EurekaInstanceConfig eurekaInstanceConfig() {
-        return new EurekaInstanceConfig();
-    }
 
 }
